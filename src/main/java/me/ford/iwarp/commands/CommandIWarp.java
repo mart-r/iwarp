27e332351cd3a1cc60de6a59a82b13ecd02ea48d
DrBot <drbot7@gmail.com>
2021-07-23 04:19:41 +0000

    Check permissions in tab completion - Helps hide commands from players who don't have permission for them while showing commands to those who do have permission.

--- src/main/java/me/ford/iwarp/commands/CommandIWarp.java
+++ src/main/java/me/ford/iwarp/commands/CommandIWarp.java
@@ -2,15 +2,12 @@ package me.ford.iwarp.commands;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
-import org.bukkit.util.StringUtil;
 
 import me.ford.iwarp.IWarpPlugin;
 import me.ford.iwarp.commands.subcommands.AbstractSubCommand;
@@ -28,8 +25,7 @@ import me.ford.iwarp.commands.subcommands.TransferCommand;
 public class CommandIWarp implements TabExecutor {
 	private final IWarpPlugin IW;
 	private final Map<String, AbstractSubCommand> subCommands = new HashMap<>();
-	private final Set<String> subCommandNames = new HashSet<>();
-	
+
 	public CommandIWarp(IWarpPlugin plugin) {
 		IW = plugin;
 		subCommands.put("create", new CreateCommand(IW));
@@ -42,26 +38,52 @@ public class CommandIWarp implements TabExecutor {
 		subCommands.put("reload", new ReloadCommand(IW));
 		subCommands.put("changeexpiration", new ChangeExpirationCommand(IW));
 		subCommands.put("arename", new AdminRenameCommand(IW));
-		subCommandNames.addAll(subCommands.keySet());
-		subCommandNames.remove("reload"); // most people can't reload
-		subCommandNames.add("help"); // I can tabcomplete, but it just returns false and displays usage
-		subCommandNames.remove("changeexpiration"); // most people can't use this
-		subCommandNames.remove("arename"); // most people can't use this
 	}
 
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
 		switch (args.length) {
-		case 0:
-			return new ArrayList<>();
-		case 1:
-			return StringUtil.copyPartialMatches(args[0], subCommandNames, new ArrayList<>());
-		default:
-			AbstractSubCommand sbc = subCommands.get(args[0].toLowerCase());
-			if (sbc == null) {
+			case 0:
 				return new ArrayList<>();
-			}
-			return sbc.onTabComplete(sender, command, label, args);
+			case 1:
+				ArrayList<String> list = new ArrayList<>();
+				if (sender.hasPermission("iwarp.command.create")) {
+					list.add("create");
+				}
+				if (sender.hasPermission("iwarp.command.renew")) {
+					list.add("renew");
+				}
+				if (sender.hasPermission("iwarp.command.move")) {
+					list.add("move");
+				}
+				if (sender.hasPermission("iwarp.command.rename")) {
+					list.add("rename");
+				}
+				if (sender.hasPermission("iwarp.command.transfer")) {
+					list.add("transfer");
+				}
+				if (sender.hasPermission("iwarp.command.list")) {
+					list.add("list");
+				}
+				if (sender.hasPermission("iwarp.command.info")) {
+					list.add("info");
+				}
+				if (sender.hasPermission("iwarp.command")) {
+					list.add("help");
+				}
+				if (sender.hasPermission("iwarp.command.changeexpiration")) {
+					list.add("changeexpiration");
+				}
+				if (sender.hasPermission("iwarp.command.adminrename")) {
+					list.add("arename");
+				}
+				return list;
+			default:
+				AbstractSubCommand sbc = subCommands.get(args[0].toLowerCase());
+				if (sbc == null) {
+					return new ArrayList<>();
+				}
+				return sbc.onTabComplete(sender, command, label, args);
 		}
 	}
 
